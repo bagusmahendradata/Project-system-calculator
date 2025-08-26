4b5dc23085137b8877282098ae38ec0bf8a5be03
Gunggus00 <gunggusss1007@gmail.com>
2025-08-26 10:54:00 +0800

    menambahkan fungsi opretion

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
-print("Hallow Mahendra")
+# Project calculator simple
+
+angka_1 = int(input('Masukan nilai :'))
+angka_2 = int(input('Masukan nilai :'))
+
+def opretion():
+    tambah = angka_1 + angka_2
+    print(f'Hasil :{tambah}')
+
+    kurang = angka_1 - angka_2
+    print(f'Hasil :{kurang}')
+
+    kali = angka_1 * angka_2
+    print(f'Hasil :{kali}')
+    
+    bagi = angka_1 / angka_2
+    print(f'Hasil :{bagi}')
+
+
+opretion()
